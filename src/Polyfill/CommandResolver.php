c4467a12b32c5dc0b8f945c4acd85b0876e72339
sadiq <sadiq810@gmail.com>
2023-10-13 14:54:49 +0500

    initial commit

---
+++ src/Polyfill/CommandResolver.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CKSource\CKFinderBridge\Polyfill;
+
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+
+/**
+ * Class CommandResolver
+ *
+ * @deprecated
+ */
+class CommandResolver extends \CKSource\CKFinder\CommandResolver implements ArgumentResolverInterface
+{
+}
